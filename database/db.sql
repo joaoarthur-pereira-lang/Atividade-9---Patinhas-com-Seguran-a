b95a8b6ec1beffa9804557310fe100ccad476b62
joao arthur pereira <joao_arthur-pereira@estudante.sesisenai.org.br>
2026-08-26 11:44:54 -0300

    add -  dados

--- database/db.sql
+++ database/db.sql
@@ -1,2 +1,9 @@
-CREATE DATABASE patinhas;
-USE patinhas;
+CREATE DATABASE pet_shop_do_jango;
+USE pet_shop_do_jango;
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
