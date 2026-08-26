a8e9f2770cfcc9175380c8b780f5508fcfc462cc
joao arthur pereira <joao_arthur-pereira@estudante.sesisenai.org.br>
2026-08-26 12:03:40 -0300

    add - fimcadaro

--- public/clientes/cadastrarclientes.php
+++ public/clientes/cadastrarclientes.php
@@ -22,3 +22,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastrar Cliente</title>
+    <link rel="stylesheet" href="../../style/style.css">
+</head>
+
+<body>
+
+<div class="container">
+
+    <h1>🐶 Cadastrar Cliente</h1>
+
+    <form method="POST">
+
+        <label>Nome:</label>
+        <input type="text" name="nome" required>
+
+        <label>Email:</label>
+        <input type="email" name="email" required>
+
+        <label>Senha:</label>
+        <input type="password" name="senha" required>
+
+        <button type="submit">
+            Cadastrar
+        </button>
+
+    </form>
+
+    <br>
+
+    <a href="../../index/index.php">Voltar</a>
+
+</div>
+
+</body>
+</html>
